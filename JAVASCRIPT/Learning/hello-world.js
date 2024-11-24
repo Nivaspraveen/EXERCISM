0801b143a7f5415548ef5924ae9cc237e25bf0ba
Nivaspraveen <nivaspraveen07@gmail.com>
2024-11-24 12:31:54 +0530

    Removed instructions

--- JAVASCRIPT/Learning/hello-world.js
+++ JAVASCRIPT/Learning/hello-world.js
@@ -1,27 +1,5 @@
-// Instructions
-// The classical introductory exercise. Just say "Hello, World!".
-
-// "Hello, World!" is the traditional first program for beginning programming in a new language or environment.
-
-// The objectives are simple:
-
-// Modify the provided code so that it produces the string "Hello, World!".
-// Run the test suite and make sure that it succeeds.
-// Submit your solution and check it at the website.
-// If everything goes well, you will be ready to fetch your first real exercise.
-
-// How to debug
-// When a test fails, a message is displayed describing what went wrong and for which input. You can also use the fact that any console output will be shown too. You can write to the console using:
-
-// console.log('Debug message');
-
-// Solution for Hello World....
-
-//
-// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// Solution for Hello World
 
 export function hello() {
     return 'Hello, World!';
-  }
+}
